a2bace0f8e669169cf7eef95651f683b8112708b
Vincent Emonet <vincent.emonet@gmail.com>
2021-01-31 16:11:28 +0100

    update to latest version of d2s-project-template

--- restart_virtuoso.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker-compose -f d2s-cwl-workflows/docker-compose.yaml down
-rm -rf /data/d2s-workspace/virtuoso
-docker-compose -f d2s-cwl-workflows/docker-compose.yaml up -d --build --force-recreate virtuoso drill
-sleep 2
-sudo chown -R ${USER}:${USER} /data/d2s-workspace
-cp d2s-cwl-workflows/support/virtuoso/load.sh /data/d2s-workspace/virtuoso
